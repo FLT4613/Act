688787048a27592046acddd47ca972503f16a6ed
FLT <ft4613st@gmail.com>
2018-08-16 15:22:00 +0900

    キャラクターを表示

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,10 +1,17 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState {
+  var hero = new FlxSprite();
   override public function create():Void {
     super.create();
+    hero.loadGraphic(AssetPaths.archer__png, true, 32, 32, true);
+    hero.animation.add("Idle", [0], 1, true);
+    hero.setPosition(100, 100);
+    hero.animation.play("Idle");
+    this.add(hero);
   }
 
   override public function update(elapsed:Float):Void {
