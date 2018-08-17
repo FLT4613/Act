9f67e28deda91e264f2bc94319a693a943326263
FLT <ft4613st@gmail.com>
2018-08-17 22:53:37 +0900

    FlxFSM導入

--- source/Player.hx
+++ source/Player.hx
@@ -2,15 +2,16 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.addons.util.FlxFSM;
 
 class Player extends FlxSprite {
+  public var fsm:FlxFSM<Player>;
   public function new() {
     super();
     this.loadGraphic(AssetPaths.archer__png, true, 32, 32, true);
-    this.animation.add("Idle", [0], 1, true);
+    this.animation.add("Stand", [0], 1, true);
     this.animation.add("Move", [0, 1, 2, 3], 10, true);
     this.animation.add("Attack", [5, 6, 6, 6, 6, 6, 7, 7, 7], 10, false);
-    this.animation.play("Idle");
   }
 
   override public function update(elapsed:Float) {
@@ -24,11 +25,14 @@ class Player extends FlxSprite {
       this.animation.play("Move");
     } else if(FlxG.keys.justPressed.SPACE) {
       this.animation.play("Attack");
-    } else {
-      if(this.animation.name=="Move" || this.animation.finished) {
-        this.animation.play("Idle");
+    } else if(this.animation.name=="Move" || this.animation.finished) {
+      this.animation.play("Stand");
       }
     }
   }
 
+class Stand extends FlxFSMState<Player> {
+  override public function enter(owner:Player, fsm:FlxFSM<Player>):Void {
+    owner.animation.play("Stand");
+  }
 }
